e5186083bbffbe223eb45b5950a51a7d677b8e9a
Singularity Labs <ishandutta2007@gmail.com>
2026-07-25 17:21:00 +0530

    imo_minor

--- scripts/technology/india_imo_percentile.py
+++ scripts/technology/india_imo_percentile.py
@@ -124,7 +124,7 @@ plt.annotate(
 
 # Plot customization
 plt.title(
-    "India's IMO Performance Percentile (1989-2026)\nRelative Positioning to Overall Pool Size",
+    f"India's IMO Performance Percentile ({imo_data[0][0]} - {imo_data[-1][0]})\nRelative Positioning to Overall Pool Size",
     fontsize=16,
     fontweight="bold",
     pad=15,
